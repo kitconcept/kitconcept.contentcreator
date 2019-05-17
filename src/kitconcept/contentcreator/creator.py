f18c32ec174b5a1d089fd415626b2c8269d50daa
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-05-17 05:27:38 -0300

    Review

--- src/kitconcept/contentcreator/creator.py
+++ src/kitconcept/contentcreator/creator.py
@@ -123,20 +123,6 @@ def set_exclude_from_nav(obj):
         obj.reindexObject(idxs=["exclude_from_nav"])
 
 
-def enable_tiles_behavior(portal_type):
-    """Enable Related Items behavior on the specified portal type."""
-    from plone.dexterity.interfaces import IDexterityFTI
-    from zope.component import queryUtility
-
-    fti = queryUtility(IDexterityFTI, name=portal_type)
-    behavior = 'plone.restapi.behaviors.ITiles'
-    if behavior in fti.behaviors:
-        return
-    behaviors = list(fti.behaviors)
-    behaviors.append(behavior)
-    fti.behaviors = tuple(behaviors)
-
-
 def create_item_runner(
     container,
     content_structure,
@@ -235,6 +221,11 @@ def create_item_runner(
 
             if not data.get("review_state") and obj.portal_type not in ignore_wf_types:
                 data["review_state"] = default_wf_state
+
+            # Add default tiles
+            if data.get("tiles", None) is not None:
+                pass
+                # enable_tiles_behavior(type_)
 
             # Populate image if any
             if ARCHETYPES_PRESENT and IBaseObject.providedBy(obj):
@@ -340,10 +331,6 @@ def create_item_runner(
             logger.info("{0}: created".format(path))
 
             create_portlets(obj, data.get("portlets", []))
-
-            # Enable Tiles behavior
-            if data.get("tiles", None) is not None:
-                enable_tiles_behavior(type_)
 
             # create local roles
             for user, roles in opts.get("local_roles", {}).items():
