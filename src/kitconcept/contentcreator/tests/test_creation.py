bf3b474069fec69ea120b59df3c99ac6db854d60
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-10-04 08:14:09 -0300

    Add tests

--- src/kitconcept/contentcreator/tests/test_creation.py
+++ src/kitconcept/contentcreator/tests/test_creation.py
@@ -72,6 +72,26 @@ class CreatorTestCase(unittest.TestCase):
         self.assertIn("another-image", self.portal.objectIds())
         self.assertTrue(self.portal["another-image"].image)
 
+        self.assertIn("news-item-image", self.portal.objectIds())
+        self.assertTrue(self.portal["news-item-image"].image)
+
+    def test_file_fields(self):
+        content_structure = load_json("fields_file.json", __file__)
+
+        with api.env.adopt_roles(["Manager"]):
+            create_item_runner(
+                self.portal,
+                content_structure,
+                default_lang="en",
+                default_wf_state="published",
+                base_image_path=os.path.dirname(__file__),
+            )
+        self.assertIn("an-file", self.portal.objectIds())
+        self.assertTrue(self.portal["an-file"].file)
+
+        self.assertIn("another-file", self.portal.objectIds())
+        self.assertTrue(self.portal["another-file"].file)
+
     def test_image_fields_deprecated(self):
         content_structure = load_json("fields_image.json", __file__)
 
@@ -88,6 +108,23 @@ class CreatorTestCase(unittest.TestCase):
 
         self.assertIn("another-image-deprecated", self.portal.objectIds())
         self.assertTrue(self.portal["another-image-deprecated"].image)
+
+    def test_file_fields_deprecated(self):
+        content_structure = load_json("fields_file.json", __file__)
+
+        with api.env.adopt_roles(["Manager"]):
+            create_item_runner(
+                self.portal,
+                content_structure,
+                default_lang="en",
+                default_wf_state="published",
+                base_image_path=os.path.dirname(__file__),
+            )
+        self.assertIn("an-file-deprecated", self.portal.objectIds())
+        self.assertTrue(self.portal["an-file-deprecated"].file)
+
+        self.assertIn("another-file-deprecated", self.portal.objectIds())
+        self.assertTrue(self.portal["another-file-deprecated"].file)
 
     def test_edit_if_content_already_exists(self):
         content_structure = load_json("test_content.json", __file__)
