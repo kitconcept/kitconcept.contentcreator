ba43ccb178c7338ee3d9da6d683d3ea969379806
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-08-21 09:45:59 +0200

    flake8 & black

--- src/kitconcept/contentcreator/creator.py
+++ src/kitconcept/contentcreator/creator.py
@@ -258,7 +258,7 @@ def create_item_runner(  # noqa
             # if don't we create it
             try:
                 obj = create(container, type_, id_=id_, title=title)
-            except:
+            except: # noqa
                 logger.warn(
                     "Can not create object {} ({}) in {}".format(
                         id_, type_, "/".join(container.getPhysicalPath())
