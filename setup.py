c7e133000aa13407a412fd01e2dedd3e63fb62f0
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-10-13 17:09:27 +0200

    Back to development: 2.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.contentcreator",
-    version="2.1.0",
+    version="2.1.1.dev0",
     description="Create Plone content via JSON",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
