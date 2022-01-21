38d373323a11f0b2bcc2b09e95f9c5fdd0a34e70
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-01-21 15:56:54 +0100

    Prepare for release

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
